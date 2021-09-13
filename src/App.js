e47db3f4b3ec187b94de36eb62b971f20eeac60e
Krystle May <kmay426@yahoo.com>
2021-09-13 19:37:11 -0400

    todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const tasks = [
+    {
+      chore: 'Buy groceries ',
+      id: 0
+    },
+    {
+      chore: 'Feed dogs ',      
+      id: 1
+    },
+    {
+      chore: 'Mow grass ',
+      id: 2
+    },
+    {
+      chore: 'Wash golf cart ',
+      id: 3
+    }
+  ];
+  
+return (
+    <div>
+      <h1>
+        May family to do list:
+      </h1>
+      <div className='items'>
+      {tasks[3].chore}
+      </div>
+      
     </div>
   );
 }
