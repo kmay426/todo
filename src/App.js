464c2305ff91608b841ea7709f9c3419a072796a
Krystle May <kmay426@yahoo.com>
2021-09-15 13:21:45 -0400

    yesterday

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 import './App.css';
 import Tasks from './Components/Tasks';
+import React from 'react';
 
 function App() {
-  const task = [
-    {
-      chore: 'Buy groceries ',
-      id: 0
-    },
-    {
-      chore: 'Feed dogs ',
-      id: 1
-    },
-    {
-      chore: 'Mow grass ',
-      id: 2
-    },
-    {
-      chore: 'Wash golf cart ',
-      id: 3
-    }
-  ];
   return (
     <div>
-      <h1>
-        May family to do list:
-      </h1>
-      <div className='items'>
-        <Tasks chore={task[0].chore} />
-        <Tasks chore={task[1].chore} />
-        <Tasks chore={task[2].chore} />
-        <Tasks chore={task[3].chore} />
-      </div>
-
+      <Tasks />
     </div>
   );
 }
--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { useState } from 'react';
 
-function Tasks(props) {
-    return (
-        <div>
-            <div></div>
-            <div>{props.chore}</div>
-        </div>
-    )
+function Tasks() {
+	const toDo = ['feed cats ', 'feed dogs ', 'walk dogs']
+	const [tasks, setTasks] = useState(toDo);
+	const newToDo = tasks.map(x => <p>{x}</p>)
+	console.log(tasks);
+	window.setTasks = setTasks;
+
+	return (
+		<div>
+			{newToDo}
+		</div>
+	);
 }
 
-
+// convert to class component & dynamically add items to list 
 
 export default Tasks;
