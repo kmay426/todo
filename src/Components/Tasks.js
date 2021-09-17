d04e44b9538e23a0b1e8cf1ecaef686bf6fa5671
Krystle May <kmay426@yahoo.com>
2021-09-17 12:05:59 -0400

    tasks

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -6,22 +6,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class Tasks extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { toDo: ['feed dogs ', 'feed cats ', 'walk dogs '], value: '' };
+		this.state = { toDo: ['feed dogs ', 'feed cats ', 'walk dogs '], value: '', submitted: false };
 		this.submit = this.submit.bind(this);
 		this.handleChange = this.handleChange.bind(this);
 	}
 
 	submit() {
-		this.setState({
-			toDo: this.state.toDo.concat(this.state.value), value: ''
-		});
+		this.setState({ 
+			toDo: this.state.toDo.concat(this.state.value), value: '', 
+		 });
 	}
 
 	handleChange(event) {
 		this.setState({ value: event.target.value });
-
 	}
 
+	//want error to display if button is clicked & there is no text in the input to add to the list
+
+	errorMessage() {
+		if (this.state.value === '' && this.state.submitted) {
+			return (
+				<p>Error!</p>
+			)
+		}
+	}
 
 	render() {
 		const list = this.state.toDo.map((toDo) => <li>{toDo}</li>);
@@ -43,15 +51,15 @@ class Tasks extends Component {
 					onClick={this.submit}
 					type='button'
 					class="btn btn-dark"
-					disabled={!this.state.value}
+				//disabled={!this.state.value}
 				>
 					Add Item
 				</button>
-			</div >
+			</div>
 		);
 	}
-
 }
+
 
 
 
