6dd6b940002f1892fa6d55d05de65495fd0759c2
Krystle May <kmay426@yahoo.com>
2021-09-23 09:46:42 -0400

    css

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -5,25 +5,7 @@ import Error from './Error';
 class Button extends Component {
   constructor(props) {
     super(props);
-    // this.state = { showError: false }
-
   }
-
-  // submit(event) {
-  //   if (this.props.value !== '') {
-  //     this.props.addItem(this.props.value)
-  //     this.setState({ showError: false })
-  //   }
-
-  //   if (this.props.value === '') {
-  //     this.setState({ showError: true });
-  //   }
-
-  //   if (this.props.value !== '') {
-  //     this.props.resetValue();
-  //   }
-
-  // };
 
   render() {
     return (
@@ -36,17 +18,12 @@ class Button extends Component {
         >
           Add item
         </button>
-        {/* <Error
-          value={this.props.value}
-          showError={this.state.showError}
-        /> */}
       </div>
     )
   }
 }
 
 export default Button;
-// move error rendering to form component 
 // mark items as complete by clicking on the item & it should have a strikethrough & color change 
 // add hover style to each list item 
 
--- src/Components/TaskItem.js
+++ src/Components/TaskItem.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react'
+import './TaskItem.css';
 
 class TaskItem extends Component {
+  constructor(props) {
+    super(props);
+    this.markCompleted = this.markCompleted.bind(this);
+    this.state = { completed: false }
+  }
+
+  markCompleted() {
+    this.setState({ completed: true })
+  }
+
+  //function that determines the class based on state 
+
+  taskItemClass() {
+   if (this.state.completed === true) {
+      return 'task-item completed';
+    } else {
+      return 'task-item';
+    }
+  }
 
   render() {
+    console.log(this.state.completed)
     return (
-      <li>{this.props.task}</li> 
+      <li
+        className={this.taskItemClass()}
+        onClick={this.markCompleted}
+      >
+        {this.props.task}
+      </li>
     );
   }
 }
 
 export default TaskItem;
+
+// click to remove strikethrough 
+// add due date to tasks 10/1/21   selecting a calendar 
+// list should display name & date it's due 
+// if past due-display in red 
