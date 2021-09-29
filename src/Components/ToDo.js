c6fc0be7814dfdb927f61c0453a745edb5df7b76
Krystle May <kmay426@yahoo.com>
2021-09-29 13:00:19 -0400

    sorting

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -9,6 +9,7 @@ class ToDo extends Component {
   constructor(props) {
     super(props);
     this.addItem = this.addItem.bind(this);
+    // this.sortList = this.sortList.bind(this);
     this.state = { tasks: ['Feed dogs', 'Walk dogs', 'Wash car', 'Feed cats'], date: ['2021-10-01 ', '2021-08-02 ', '2021-10-06 ', '2021-10-10 '] }
   }
 
@@ -17,14 +18,30 @@ class ToDo extends Component {
     this.setState({ tasks: this.state.tasks.concat(item), date: this.state.date.concat(date) })
   }
 
+  // sortList(event) {
+  //   this.date.sort()
+  // }
+
+  // need to sort my array prior to mapping, need it sorted by value of date & tasks sorted with it 
+
   render() {
-    console.log(this.state.tasks, this.state.date)
+    const myData = this.state.tasks.concat(this.state.date);
+    console.log(myData) //sorted tasks then dates at bottom/did not sort by date
+    this.state.date.sort()  // this.state.date.sort() is sorting the dates how we want
     return (
       <div className='container'>
-        {this.state.tasks.map((task, idx) => <TaskItem task={task} 
-        // + ' ' + this.state.date[idx]} 
-           date={this.state.date[idx]}  
-            />)}
+        <button
+          type='button'
+          class='btn btn-primary'
+          onClick={this.sortList}
+        >
+          Sort
+        </button>
+        {this.state.tasks.map((task, idx) => <TaskItem task={task}
+          // + ' ' + this.state.date[idx]} 
+          date={this.state.date[idx]}
+        />)}
+        {/* {this.state.date.sort()} */}
         <Form addItem={this.addItem} />
       </div>
     );
@@ -34,3 +51,5 @@ class ToDo extends Component {
 export default ToDo;
 
 // mapping -1st argument available is the element and 2nd argument is the index 
+
+// start with adding a button onclick will make it sort the list-items earliest due at the top 
